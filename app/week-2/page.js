d13b83668c0b171de6b7107b4756abba75c63c21
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2024-07-07 13:16:59 -0600

    student info page
    added student info and imported it to page.js file

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,13 @@
-export default function Page() {
+import Link from "next/link";
+import StudentInfo from "./student-info";
+
+export default function Page2() {
     return (
-      <main>
-        <h1>Shopping List</h1>
-      </main>
+        <main>
+            <h1>My Second Page!</h1>
+            <p>This is the second page of my React App!</p>
+            <StudentInfo />
+            <Link href="../">Back</Link>
+        </main>
     );
-  }
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Viclei Rwyn Pasco</h1>
+      <Link href="https://github.com/VicleiPasco">
+        <a>GitHub Repository</a>
+      </Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
