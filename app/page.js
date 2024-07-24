b5534f9768b79b16d45a03a4a917208a22799dd2
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2024-07-24 13:04:20 -0600

    Added week-4
    added counter, functions, and managed-form in the week-4 folder. Added link to week-4 in page.js

--- app/page.js
+++ app/page.js
@@ -11,8 +11,11 @@ export default function Home() {
       <p>Shopping List</p>
       <h2>Application Links</h2>
       <ul>
-        <li><Link className={linkStyles} href="./week2/">Week 2</Link></li>
-        <li><Link className={linkStyles} href="./week-3/page.js">Week 3</Link></li>
+        <li><Link className={linkStyles} href="./week-2/">Week 2</Link></li>
+        <li><Link className={linkStyles} href="./week-3/">Week 3</Link></li>
+        <li><Link className={linkStyles} href="./week-4/functions">Week 4 - Functions</Link></li>
+        <li><Link className={linkStyles} href="./week-4/counter">Week 4 - Counter</Link></li>
+        <li><Link className={linkStyles} href="./week-4/managed-form">Week 4 - Dog Form</Link></li>
       </ul>
     </main>
   );
